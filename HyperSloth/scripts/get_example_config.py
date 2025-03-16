adbec4cffa9925d9c9b81adce8d6c6200a044702
anhvth <anhvth.226@gmail.com>
2025-03-16 06:52:27 +0000

    Add example training configuration and update config retrieval script

--- HyperSloth/scripts/get_example_config.py
+++ HyperSloth/scripts/get_example_config.py
@@ -2,7 +2,9 @@ from fastcore.all import *
 
 
 
-def main():
-    path = Path(__file__).parent.parent.parent / "example_training_config.py"
+def main(out_path='./hs_training_config.py'):
+    path = Path(__file__).parent.parent / "example_training_config.py"
     import os
-    os.system(f"cp {path} ./hs_training_config.py")
+    os.system(f"cp {path} {out_path}")
+    print(f"Example training config: {out_path}")
+    
