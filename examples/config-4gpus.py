1a2afa87c10931039ff1392e68813276bfd42e83
anh vo <anhvth.226@gmail.com>
2025-03-23 17:23:52 +0000

    Update training configuration: enable length grouping, increase batch size, and adjust gradient accumulation steps

--- examples/config-4gpus.py
+++ examples/config-4gpus.py
@@ -5,7 +5,7 @@ hyper_config_model = HyperConfig(
     data=DataConfig(
         dataset_name_or_path="data/alpaca-cleaned",
         split="train",
-        group_by_length=False,
+        group_by_length=True,
         instruction_part='<start_of_turn>user\n',
         response_part="<start_of_turn>model\n",
         num_samples=16_000,
@@ -27,8 +27,8 @@ hyper_config_model = HyperConfig(
 # Training arguments using Pydantic model
 training_config_model = TrainingArgsConfig(
     output_dir="/data-4090/anhvth5/hypersloth_output/loras/gemma-3-4b-it/alpaca-cleaned-4gpus",
-    per_device_train_batch_size=4,  
-    gradient_accumulation_steps=8,  # Meaing 8*4*4=128 examples per step
+    per_device_train_batch_size=8,  
+    gradient_accumulation_steps=16,  # Meaing 8*4*4=128 examples per step
     learning_rate=2e-4,
     per_device_eval_batch_size=4,
     eval_steps=100000,
