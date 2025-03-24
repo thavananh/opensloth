3f7a73b12b8643af2e075ec2c338d9cfdf332502
anh vo <anhvth.226@gmail.com>
2025-03-24 05:40:08 +0000

    Update model name and training configuration in config-4gpus.py

--- examples/config-4gpus.py
+++ examples/config-4gpus.py
@@ -15,7 +15,7 @@ hyper_config_model = HyperConfig(
         loss_type="response_only",  # Choices: ["all", "response_only"], the loss will only be calculated on the response part of the input
     ),
     fast_model_args=FastModelArgs(
-        model_name="unsloth/gemma-3-4b-it-bnb-4bit",
+        model_name="unsloth/gemma-3-27b-it-bnb-4bit",
         max_seq_length=4_000,
     ),
     lora_args=LoraArgs(
@@ -26,9 +26,9 @@ hyper_config_model = HyperConfig(
 
 # Training arguments using Pydantic model
 training_config_model = TrainingArgsConfig(
-    output_dir="/data-4090/anhvth5/hypersloth_output/loras/gemma-3-4b-it/alpaca-cleaned-4gpus",
-    per_device_train_batch_size=8,  
-    gradient_accumulation_steps=4,  # Meaing 8*4*4=128 examples per step
+    output_dir="/data-4090/anhvth5/hypersloth_output/loras/gemma-3-27b-it/alpaca-cleaned-4gpus",
+    per_device_train_batch_size=1,  
+    gradient_accumulation_steps=32,  # Meaing 8*4*4=128 examples per step
     learning_rate=2e-4,
     per_device_eval_batch_size=4,
     eval_steps=100000,
