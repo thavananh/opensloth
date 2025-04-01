7aabc371f20744bbf9e92c547fd0c437f38a7fa6
anh vo <anhvth.226@gmail.com>
2025-04-01 04:12:11 +0000

    update

--- examples/train_cfg.py
+++ examples/train_cfg.py
@@ -10,12 +10,12 @@ hyper_config_model = HyperConfig(
         response_part="<|im_start|>assistant\n",
     ),
     training=TrainingConfig(
-        gpus=range(1),
+        gpus=range(8),
         loss_type="response_only",  # Choices: ["all", "response_only"], the loss will only be calculated on the response part of the input
-        # chat_template='Qwen/Qwen2.5-32B-Instruct'
+        # chat_template='Qwen/Qwen2.5-7B-Instruct'
     ),
     fast_model_args=FastModelArgs(
-        model_name="unsloth/Qwen2.5-32B-Instruct-bnb-4bit",
+        model_name="unsloth/Qwen2.5-32B-Instruct",
         max_seq_length=7_000,
     ),
     # pretrained_lora="/shared-mnt/loras/gemma-3-27b-it-bnb-4bit_teacher_messages_deepseek_direct/loss_response_only_lora_r16_a16_seq_7000_lr_0_0001_global_bz_16_epochs_2_seed_42_mmap/",
@@ -30,7 +30,7 @@ hyper_config_model = HyperConfig(
 training_config_model = TrainingArgsConfig(
     output_dir="/shared-mnt/loras/",
     per_device_train_batch_size=1,
-    gradient_accumulation_steps=16,  # Meaing 8*4*4=128 examples per step
+    gradient_accumulation_steps=2,  # Meaing 8*4*4=128 examples per step
     num_train_epochs=1,
     learning_rate=1e-4,
     eval_steps=100000,
